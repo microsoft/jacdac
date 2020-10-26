9e88c7e3b72c8eb19a4d7dd5ea5c17ac12f50909
peli <pelikhan@users.noreply.github.com>
2020-10-26 01:50:58 -0700

    adding list of unit from SenML

--- spectool/jdspec.d.ts
+++ spectool/jdspec.d.ts
@@ -11,10 +11,19 @@ declare namespace jdspec {
      * Unit for a field.
      * 
      * 'frac' means that the range of the storage type is supposed to cover 0 to 1 (or -1 to 1 for signed types).
-     * 
      * Many fields have no unit (eg. because they represent counts); in that case empty string is used.
+     * SenML (https://www.iana.org/assignments/senml/senml.xhtml) units and secondary unit are also supported.
      */
-    type Unit = "" | "frac" | "s" | "ms" | "us" | "mV" | "mA" | "mWh" | "C" | "K" | "g" | "%RH" | "bytes"
+    type Unit = "" | "frac" | "us" | "mWh" | "bytes" | "grav"
+    // SenML units
+    | "m" | "kg" | "g" | "s" | "A" | "K" | "cd" | "mol" | "Hz" | "rad" | "sr" | "N" | "Pa" | "J" | "W" | "C" | "V" | "F" | "Ohm"
+    | "S" | "Wb" | "T" | "H" | "Cel" | "lm" | "lx" | "Bq" | "Gy" | "Sv" | "kat" | "m2" | "m3" | "l" | "m/s" | "m/s2" | "m3/s" | "l/s"
+    | "W/m2" | "cd/m2" | "bit" | "bit/s" | "lat" | "lon" | "pH" | "dB" | "dBW" | "Bspl" | "count" | "/" | "%" | "%RH" | "%EL" | "EL"
+    | "1/s" | "1/min" | "beat/min" | "beats" | "S/m" | "B" | "VA" | "VAs" | "var" | "vars" | "J/m" | "kg/m3" | "deg"
+    // SenML secondary units
+    | "ms" | "min" | "h" | "MHz" | "kW" | "kVA" | "kvar" | "Ah" | "Wh" | "kWh"
+    | "varh" | "kvarh" | "kVAh" | "Wh/km" | "KiB" | "GB" | "Mbit/s" | "B/s" | "MB/s" | "mV" | "mA" | "dBm" | "ug/m3"
+    | "mm/h" | "m/h" | "ppm" | "/100" | "/1000" | "hPa" | "mm" | "cm" | "km" | "km/h";
 
     /**
      * Service specification.

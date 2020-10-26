de77803010b6a3499975de75d72cfac69260826f
peli <pelikhan@users.noreply.github.com>
2020-10-26 03:58:16 -0700

    use count unit for crank

--- dist/specconstants.ts
+++ dist/specconstants.ts
@@ -249,12 +249,12 @@ export enum CtrlReg {
 export const SRV_ROTARY_ENCODER = 0x10fa29c9
 export enum RotaryEncoderReg {
     /**
-     * Read-only int32_t. Upon device reset starts at `0` (regardless of the shaft position).
+     * Read-only # int32_t. Upon device reset starts at `0` (regardless of the shaft position).
      * Increases by `1` for a clockwise "click", by `-1` for counter-clockwise.
      */
     Position = 0x101,
     
-    /** Constant uint16_t. This specifies by how much `position` changes when the crank does 360 degree turn. Typically 12 or 24. */
+    /** Constant # uint16_t. This specifies by how much `position` changes when the crank does 360 degree turn. Typically 12 or 24. */
     ClicksPerTurn = 0x180,
 }
 

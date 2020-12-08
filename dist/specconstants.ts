a879473d43dbb8fbc4b3fd44cffd8932e44a8a7e
Peli de Halleux <peli@Pelis-MacBook-Pro.local>
2020-12-08 10:30:07 +0100

    temperature -> thermometer

--- dist/specconstants.ts
+++ dist/specconstants.ts
@@ -1474,9 +1474,9 @@ export enum TCPPipeCmd {
     Error = 0x0,
 }
 
-// Service: Temperature
-export const SRV_TEMPERATURE = 0x1421bac7
-export enum TemperatureReg {
+// Service: Thermometer
+export const SRV_THERMOMETER = 0x1421bac7
+export enum ThermometerReg {
     /**
      * Read-only °C u22.10 (uint32_t). The temperature.
      */

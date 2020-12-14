d6d06202eb7ee134e5249a0e759e6452f9326bf3
Michal Moskal <michal@moskal.me>
2020-12-14 19:05:59 +0100

    Add QoS counter; fixes #90

--- dist/specconstants.ts
+++ dist/specconstants.ts
@@ -385,6 +385,8 @@ export enum ControlCmd {
      * The upper 4 bits of `restart_counter` are reserved.
      * `service_class` indicates class identifier for each service index (service index `0` is always control, so it's
      * skipped in this enumeration).
+     * `packet_count` indicates the number of packets sent by the current device since last announce,
+     * including the current announce packet (it is always 0 if this feature is not supported).
      * The command form can be used to induce report, which is otherwise broadcast every 500ms.
      */
     Services = 0x0,
@@ -392,7 +394,7 @@ export enum ControlCmd {
     /**
      * report Services
      * ```
-     * const [restartCounter, flags, serviceClass] = jdunpack<[number, ControlAnnounceFlags, number[]]>(buf, "u8 u8 x[2] u32[]")
+     * const [restartCounter, flags, packetCount, serviceClass] = jdunpack<[number, ControlAnnounceFlags, number, number[]]>(buf, "u8 u8 u8 x[1] u32[]")
      * ```
      */
 
@@ -410,6 +412,24 @@ export enum ControlCmd {
      * No args. Reset device. ACK may or may not be sent.
      */
     Reset = 0x82,
+
+    /**
+     * The device will respond `num_responses` times, as fast as it can, setting the `counter` field in the report
+     * to `start_counter`, then `start_counter + 1`, ..., and finally `start_counter + num_responses - 1`.
+     * The `dummy_payload` is `size` bytes long and contains bytes `0, 1, 2, ...`.
+     *
+     * ```
+     * const [numResponses, startCounter, size] = jdunpack<[number, number, number]>(buf, "u32 u32 u8")
+     * ```
+     */
+    FloodPing = 0x83,
+
+    /**
+     * report FloodPing
+     * ```
+     * const [counter, dummyPayload] = jdunpack<[number, UInt8Array]>(buf, "u32 b")
+     * ```
+     */
 }
 
 export enum ControlReg {

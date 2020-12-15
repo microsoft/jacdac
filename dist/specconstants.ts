1f03177ca15a1171627f306d09070f75d1e6c784
Peli de Halleux <pelikhan@users.noreply.github.com>
2020-12-15 15:25:11 +0100

    Mono light (#109)
    * renaming service
    
    * Apply automatic changes

--- dist/specconstants.ts
+++ dist/specconstants.ts
@@ -1360,6 +1360,68 @@ export enum ModelRunnerReg {
     Parallel = 0x188,
 }
 
+// Service: Mono Light
+export const SRV_MONO_LIGHT = 0x1fb57453
+export enum MonoLightReg {
+    /**
+     * Read-write ratio uint16_t. Set the luminosity of the strip. The value is used to scale `start_intensity` in `steps` register.
+     * At `0` the power to the strip is completely shut down.
+     *
+     * ```
+     * const [brightness] = jdunpack<[number]>(buf, "u16")
+     * ```
+     */
+    Brightness = 0x1,
+
+    /**
+     * Read-write mA uint16_t. Limit the power drawn by the light-strip (and controller).
+     *
+     * ```
+     * const [maxPower] = jdunpack<[number]>(buf, "u16")
+     * ```
+     */
+    MaxPower = 0x7,
+
+    /**
+     * Constant uint8_t. Maximum number of steps allowed in animation definition. This determines the size of the `steps` register.
+     *
+     * ```
+     * const [maxSteps] = jdunpack<[number]>(buf, "u8")
+     * ```
+     */
+    MaxSteps = 0x180,
+
+    /**
+     * The steps of current animation. Setting this also sets `current_iteration` to `0`.
+     * Step with `duration == 0` is treated as an end marker.
+     *
+     * ```
+     * const [rest] = jdunpack<[([number, number])[]]>(buf, "r: u16 u16")
+     * const [startIntensity, duration] = rest[0]
+     * ```
+     */
+    Steps = 0x82,
+
+    /**
+     * Read-write uint16_t. Currently excecuting iteration of animation. Can be set to `0` to restart current animation.
+     * If `current_iteration > max_iterations`, then no animation is currently running.
+     *
+     * ```
+     * const [currentIteration] = jdunpack<[number]>(buf, "u16")
+     * ```
+     */
+    CurrentIteration = 0x80,
+
+    /**
+     * Read-write uint16_t. The animation will be repeated `max_iterations + 1` times.
+     *
+     * ```
+     * const [maxIterations] = jdunpack<[number]>(buf, "u16")
+     * ```
+     */
+    MaxIterations = 0x81,
+}
+
 // Service: Motor
 export const SRV_MOTOR = 0x17004cd8
 export enum MotorReg {
@@ -1773,68 +1835,6 @@ export enum ProtoTestCmd {
      * ```
      */
     CBytes = 0x85,
-}
-
-// Service: PWM Light
-export const SRV_PWM_LIGHT = 0x1fb57453
-export enum PwmLightReg {
-    /**
-     * Read-write ratio uint16_t. Set the luminosity of the strip. The value is used to scale `start_intensity` in `steps` register.
-     * At `0` the power to the strip is completely shut down.
-     *
-     * ```
-     * const [brightness] = jdunpack<[number]>(buf, "u16")
-     * ```
-     */
-    Brightness = 0x1,
-
-    /**
-     * Read-write mA uint16_t. Limit the power drawn by the light-strip (and controller).
-     *
-     * ```
-     * const [maxPower] = jdunpack<[number]>(buf, "u16")
-     * ```
-     */
-    MaxPower = 0x7,
-
-    /**
-     * Constant uint8_t. Maximum number of steps allowed in animation definition. This determines the size of the `steps` register.
-     *
-     * ```
-     * const [maxSteps] = jdunpack<[number]>(buf, "u8")
-     * ```
-     */
-    MaxSteps = 0x180,
-
-    /**
-     * The steps of current animation. Setting this also sets `current_iteration` to `0`.
-     * Step with `duration == 0` is treated as an end marker.
-     *
-     * ```
-     * const [rest] = jdunpack<[([number, number])[]]>(buf, "r: u16 u16")
-     * const [startIntensity, duration] = rest[0]
-     * ```
-     */
-    Steps = 0x82,
-
-    /**
-     * Read-write uint16_t. Currently excecuting iteration of animation. Can be set to `0` to restart current animation.
-     * If `current_iteration > max_iterations`, then no animation is currently running.
-     *
-     * ```
-     * const [currentIteration] = jdunpack<[number]>(buf, "u16")
-     * ```
-     */
-    CurrentIteration = 0x80,
-
-    /**
-     * Read-write uint16_t. The animation will be repeated `max_iterations + 1` times.
-     *
-     * ```
-     * const [maxIterations] = jdunpack<[number]>(buf, "u16")
-     * ```
-     */
-    MaxIterations = 0x81,
 }
 
 // Service: Role Manager

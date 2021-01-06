40ad6bac84bb1d2fce1a19d69c7170b921dd4f23
Michal Moskal <michal@moskal.me>
2021-01-06 17:30:28 +0100

    Add control.reset_in registers (watchdog)

--- dist/specconstants.ts
+++ dist/specconstants.ts
@@ -517,6 +517,17 @@ export enum ControlCmd {
 
 export enum ControlReg {
     /**
+     * Read-write μs uint32_t. When set to value other than `0`, it asks the device to reset after specified number of microseconds.
+     * This is typically used to implement watchdog functionality, where a brain device sets `reset_in` to
+     * say 1.6s every 0.5s.
+     *
+     * ```
+     * const [resetIn] = jdunpack<[number]>(buf, "u32")
+     * ```
+     */
+    ResetIn = 0x80,
+
+    /**
      * Constant string (bytes). Identifies the type of hardware (eg., ACME Corp. Servo X-42 Rev C)
      *
      * ```

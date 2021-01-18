a751f8ea31f151d72fc4573a990ec8cd88aa61f2
pelikhan <jhalleux@microsoft.com>
2021-01-18 17:46:39 +0100

    add barometer spec

--- dist/specconstants.ts
+++ dist/specconstants.ts
@@ -366,6 +366,28 @@ export enum SensorAggregatorReg {
     CurrentSample = 0x101,
 }
 
+// Service: Barometer
+export const SRV_BAROMETER = 0x1e117cea
+export enum BarometerReg {
+    /**
+     * Read-only hPa u22.10 (uint32_t). The air pressure.
+     *
+     * ```
+     * const [pressure] = jdunpack<[number]>(buf, "u22.10")
+     * ```
+     */
+    Pressure = 0x101,
+
+    /**
+     * Read-only hPa u22.10 (uint32_t). The real pressure is between `pressure - pressure_error` and `pressure + pressure_error`.
+     *
+     * ```
+     * const [pressureError] = jdunpack<[number]>(buf, "u22.10")
+     * ```
+     */
+    PressureError = 0x106,
+}
+
 // Service: Bootloader
 export const SRV_BOOTLOADER = 0x1ffa9948
 

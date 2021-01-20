afed1c603670149e3e2db5b117d0b6f1bbd9cc5e
peli <pelikhan@users.noreply.github.com>
2021-01-20 06:24:03 -0800

    adding traffic light

--- dist/specconstants.ts
+++ dist/specconstants.ts
@@ -2606,6 +2606,37 @@ export enum ThermometerReg {
     Variant = 0x107,
 }
 
+// Service: Traffic Light
+export const SRV_TRAFFIC_LIGHT = 0x15c38d9b
+export enum TrafficLightReg {
+    /**
+     * Read-write bool (uint8_t). The on/off state of the red light.
+     *
+     * ```
+     * const [red] = jdunpack<[number]>(buf, "u8")
+     * ```
+     */
+    Red = 0x80,
+
+    /**
+     * Read-write bool (uint8_t). The on/off state of the red light.
+     *
+     * ```
+     * const [orange] = jdunpack<[number]>(buf, "u8")
+     * ```
+     */
+    Orange = 0x81,
+
+    /**
+     * Read-write bool (uint8_t). The on/off state of the red light.
+     *
+     * ```
+     * const [green] = jdunpack<[number]>(buf, "u8")
+     * ```
+     */
+    Green = 0x82,
+}
+
 // Service: Vibration motor
 export const SRV_VIBRATION_MOTOR = 0x183fc4a2
 export enum VibrationMotorReg {
---
+++ services/trafficlight.md
@@ -0,0 +1,19 @@
+# Traffic Light
+
+Controls a mini traffic with a red, orange and green LED.
+
+        identifier: 0x15c38d9b
+
+## Registers
+
+    rw red: bool @ 0x80
+
+The on/off state of the red light.
+
+    rw orange: bool @ 0x81
+
+The on/off state of the red light.
+
+    rw green: bool @ 0x82
+
+The on/off state of the red light.

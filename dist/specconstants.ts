a2786de839655a4645f422e1a0900109604625e9
pelikhan <pelikhan@users.noreply.github.com>
2021-01-21 20:19:00 +0000

    Apply automatic changes

--- dist/specconstants.ts
+++ dist/specconstants.ts
@@ -2976,3 +2976,34 @@ export enum WifiEvent {
     LostIp = 0x2,
 }
 
+// Service: Wind speed
+export const SRV_WIND_SPEED = 0x1b591bbf
+export enum WindSpeedReg {
+    /**
+     * Read-only m/s u16.16 (uint32_t). The velocity of the wind.
+     *
+     * ```
+     * const [windSpeed] = jdunpack<[number]>(buf, "u16.16")
+     * ```
+     */
+    WindSpeed = 0x101,
+
+    /**
+     * Read-only m/s u16.16 (uint32_t). Error on the reading
+     *
+     * ```
+     * const [windSpeedError] = jdunpack<[number]>(buf, "u16.16")
+     * ```
+     */
+    WindSpeedError = 0x106,
+
+    /**
+     * Constant m/s u16.16 (uint32_t). Maximum speed that can be measured by the sensor.
+     *
+     * ```
+     * const [maxWindSpeed] = jdunpack<[number]>(buf, "u16.16")
+     * ```
+     */
+    MaxWindSpeed = 0x105,
+}
+

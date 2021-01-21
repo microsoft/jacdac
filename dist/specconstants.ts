66e119676002cbea7c37c95ce8087159c1b51711
pelikhan <pelikhan@users.noreply.github.com>
2021-01-21 20:17:08 +0000

    Apply automatic changes

--- dist/specconstants.ts
+++ dist/specconstants.ts
@@ -1458,6 +1458,85 @@ export enum LoggerCmd {
     Error = 0x83,
 }
 
+// Service: Matrix Keypad
+export const SRV_MATRIX_KEYPAD = 0x13062dc8
+export enum MatrixKeypadReg {
+    /**
+     * Read-only. The coordinate of the button currently pressed. Keys are zero-indexed from left to right, top to bottom:
+     * ``row = index / columns``, ``column = index % columns``.
+     *
+     * ```
+     * const [index] = jdunpack<[number[]]>(buf, "u8[]")
+     * ```
+     */
+    Pressed = 0x101,
+
+    /**
+     * Constant uint8_t. Number of rows in the matrix
+     *
+     * ```
+     * const [rows] = jdunpack<[number]>(buf, "u8")
+     * ```
+     */
+    Rows = 0x180,
+
+    /**
+     * Constant uint8_t. Number of columns in the matrix
+     *
+     * ```
+     * const [columns] = jdunpack<[number]>(buf, "u8")
+     * ```
+     */
+    Columns = 0x181,
+
+    /**
+     * Constant. The characters printed on the keys if any, in indexing sequence.
+     *
+     * ```
+     * const [label] = jdunpack<[string[]]>(buf, "s[]")
+     * ```
+     */
+    Labels = 0x182,
+}
+
+export enum MatrixKeypadEvent {
+    /**
+     * Argument: uint8_t. Emitted when a key, at the given index, goes from inactive (`pressed == 0`) to active.
+     *
+     * ```
+     * const [down] = jdunpack<[number]>(buf, "u8")
+     * ```
+     */
+    Down = 0x1,
+
+    /**
+     * Argument: uint8_t. Emitted when a key, at the given index, goes from active (`pressed == 1`) to inactive.
+     *
+     * ```
+     * const [up] = jdunpack<[number]>(buf, "u8")
+     * ```
+     */
+    Up = 0x2,
+
+    /**
+     * Argument: uint8_t. Emitted together with `up` when the press time was not longer than 500ms.
+     *
+     * ```
+     * const [click] = jdunpack<[number]>(buf, "u8")
+     * ```
+     */
+    Click = 0x80,
+
+    /**
+     * Argument: uint8_t. Emitted together with `up` when the press time was more than 500ms.
+     *
+     * ```
+     * const [longClick] = jdunpack<[number]>(buf, "u8")
+     * ```
+     */
+    LongClick = 0x81,
+}
+
 // Service: Microphone
 export const SRV_MICROPHONE = 0x113dac86
 export enum MicrophoneCmd {

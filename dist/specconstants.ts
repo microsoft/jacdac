3480057b6b88a1235e98645eadcb63b3b4d607e8
pelikhan <pelikhan@users.noreply.github.com>
2021-01-22 23:14:03 +0000

    Apply automatic changes

--- dist/specconstants.ts
+++ dist/specconstants.ts
@@ -2754,6 +2754,34 @@ export enum SettingsEvent {
     Change = 0x3,
 }
 
+// Service: Soil moisture
+export const SRV_SOIL_MOISTURE = 0x1d4aa3b3
+
+export enum SoilMoistureVariant { // uint8_t
+    Resistive = 0x1,
+    Capacitive = 0x2,
+}
+
+export enum SoilMoistureReg {
+    /**
+     * Read-only ratio uint16_t. Indicates the wetness of the soil, from ``dry`` to ``wet``.
+     *
+     * ```
+     * const [moisture] = jdunpack<[number]>(buf, "u16")
+     * ```
+     */
+    Moisture = 0x101,
+
+    /**
+     * Constant Variant (uint8_t). Describe the type of physical sensor.
+     *
+     * ```
+     * const [variant] = jdunpack<[SoilMoistureVariant]>(buf, "u8")
+     * ```
+     */
+    Variant = 0x107,
+}
+
 // Service: Switch
 export const SRV_SWITCH = 0x1ad29402
 

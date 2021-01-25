fd17b9085a8e9c28b2b3fd061850c5cb5707f441
Peli de Halleux <pelikhan@users.noreply.github.com>
2021-01-25 21:50:07 +0100

    Service: UV index (#192)
    * added service files
    
    * Update uvindex.md
    
    * pr feedback

--- dist/specconstants.ts
+++ dist/specconstants.ts
@@ -3205,6 +3205,43 @@ export enum TVOCReg {
     ConditioningPeriod = 0x180,
 }
 
+// Service: UV index
+export const SRV_U_VINDEX = 0x1f6e0d90
+
+export enum UVIndexVariant { // uint8_t
+    UVA_UVB = 0x1,
+    Visible_IR = 0x2,
+}
+
+export enum UVIndexReg {
+    /**
+     * Read-only uv u16.16 (uint32_t). Ultraviolet index, typically refreshed every second.
+     *
+     * ```
+     * const [uvIndex] = jdunpack<[number]>(buf, "u16.16")
+     * ```
+     */
+    UvIndex = 0x101,
+
+    /**
+     * Read-only uv u16.16 (uint32_t). Error on the UV measure.
+     *
+     * ```
+     * const [uvIndexError] = jdunpack<[number]>(buf, "u16.16")
+     * ```
+     */
+    UvIndexError = 0x106,
+
+    /**
+     * Constant Variant (uint8_t). The type of physical sensor and capabilities.
+     *
+     * ```
+     * const [variant] = jdunpack<[UVIndexVariant]>(buf, "u8")
+     * ```
+     */
+    Variant = 0x107,
+}
+
 // Service: Vibration motor
 export const SRV_VIBRATION_MOTOR = 0x183fc4a2
 export enum VibrationMotorReg {

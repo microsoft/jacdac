5cc027332f35570831e8be93caab66abbcfd9f5a
pelikhan <jhalleux@microsoft.com>
2021-01-25 09:17:48 +0100

    fix tvoc unit

--- dist/specconstants.ts
+++ dist/specconstants.ts
@@ -3075,6 +3075,55 @@ export enum TrafficLightReg {
     Green = 0x82,
 }
 
+// Service: Total Volatile organic compound
+export const SRV_TVOC = 0x12a5b597
+export enum TVOCReg {
+    /**
+     * Read-only ppb u22.10 (uint32_t). Total volatile organic compound readings in parts per billion.
+     *
+     * ```
+     * const [tVOC] = jdunpack<[number]>(buf, "u22.10")
+     * ```
+     */
+    TVOC = 0x101,
+
+    /**
+     * Read-only ppb u22.10 (uint32_t). Error on the reading data
+     *
+     * ```
+     * const [tVOCError] = jdunpack<[number]>(buf, "u22.10")
+     * ```
+     */
+    TVOCError = 0x106,
+
+    /**
+     * Constant ppb u22.10 (uint32_t). Minimum measurable value
+     *
+     * ```
+     * const [min_TVOC] = jdunpack<[number]>(buf, "u22.10")
+     * ```
+     */
+    Min_TVOC = 0x104,
+
+    /**
+     * Constant ppb u22.10 (uint32_t). Minimum measurable value
+     *
+     * ```
+     * const [max_TVOC] = jdunpack<[number]>(buf, "u22.10")
+     * ```
+     */
+    Max_TVOC = 0x105,
+
+    /**
+     * Constant s uint32_t. Time required to achieve good sensor stability before measuring after long idle period.
+     *
+     * ```
+     * const [conditioningPeriod] = jdunpack<[number]>(buf, "u32")
+     * ```
+     */
+    ConditioningPeriod = 0x180,
+}
+
 // Service: Vibration motor
 export const SRV_VIBRATION_MOTOR = 0x183fc4a2
 export enum VibrationMotorReg {
@@ -3109,55 +3158,6 @@ export enum VibrationMotorCmd {
      * ```
      */
     Vibrate = 0x80,
-}
-
-// Service: Volatile organic compound
-export const SRV_VOC = 0x12a5b597
-export enum VocReg {
-    /**
-     * Read-only ppm u22.10 (uint32_t). Total volatile organic compound readings.
-     *
-     * ```
-     * const [vOC] = jdunpack<[number]>(buf, "u22.10")
-     * ```
-     */
-    VOC = 0x101,
-
-    /**
-     * Read-only ppm u22.10 (uint32_t). Error on the reading data
-     *
-     * ```
-     * const [vOCError] = jdunpack<[number]>(buf, "u22.10")
-     * ```
-     */
-    VOCError = 0x106,
-
-    /**
-     * Constant ppm u22.10 (uint32_t). Minimum measurable value
-     *
-     * ```
-     * const [min_VOC] = jdunpack<[number]>(buf, "u22.10")
-     * ```
-     */
-    Min_VOC = 0x104,
-
-    /**
-     * Constant ppm u22.10 (uint32_t). Minimum measurable value
-     *
-     * ```
-     * const [max_VOC] = jdunpack<[number]>(buf, "u22.10")
-     * ```
-     */
-    Max_VOC = 0x105,
-
-    /**
-     * Constant s uint32_t. Time required to achieve good sensor stability before measuring after long idle period.
-     *
-     * ```
-     * const [conditioningPeriod] = jdunpack<[number]>(buf, "u32")
-     * ```
-     */
-    ConditioningPeriod = 0x180,
 }
 
 // Service: WIFI

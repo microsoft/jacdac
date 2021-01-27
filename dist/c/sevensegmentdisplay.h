e2ab6d744d2bea28074d83feebbc2b2f053c6048
mmoskal <mmoskal@users.noreply.github.com>
2021-01-27 23:36:11 +0000

    Apply automatic changes

--- dist/c/sevensegmentdisplay.h
+++ dist/c/sevensegmentdisplay.h
@@ -5,8 +5,10 @@
 #define JD_SERVICE_CLASS_SEVEN_SEGMENT_DISPLAY  0x196158f7
 
 /**
- * Read-write bytes. Each byte encodes the display status of a digit using a ``GFEDCBA`` encoding. 
- * If incoming ``digits`` data is smaller than available, the remaining digits will be cleared. Thus, sending an empty ``digits`` payload clears the screen.
+ * Read-write bytes. Each byte encodes the display status of a digit using, 
+ * where bit 0 encodes segment `A`, bit 1 encodes segments `B`, ..., bit 6 encodes segments `G`, and bit 7 encodes the decimal point (if present).
+ * If incoming ``digits`` data is smaller than `digit_count`, the remaining digits will be cleared.
+ * Thus, sending an empty ``digits`` payload clears the screen.
  */
 #define JD_SEVEN_SEGMENT_DISPLAY_REG_DIGITS JD_REG_VALUE
 
@@ -16,13 +18,19 @@
 #define JD_SEVEN_SEGMENT_DISPLAY_REG_BRIGHTNESS JD_REG_INTENSITY
 
 /**
- * Read-write bool (uint8_t). Turn on or off the column leds in of the segment. If the column leds is not supported, the value remains false.
+ * Read-write bool (uint8_t). Turn on or off the column LEDs (separating minutes from hours, etc.) in of the segment.
+ * If the column LEDs is not supported, the value remains false.
  */
 #define JD_SEVEN_SEGMENT_DISPLAY_REG_DOUBLE_DOTS 0x80
 
 /**
  * Constant uint8_t. The number of digits available on the display.
  */
-#define JD_SEVEN_SEGMENT_DISPLAY_REG_DIGIT_COUNT 0x80
+#define JD_SEVEN_SEGMENT_DISPLAY_REG_DIGIT_COUNT 0x180
+
+/**
+ * Constant bool (uint8_t). True if decimal points are available (on all digits).
+ */
+#define JD_SEVEN_SEGMENT_DISPLAY_REG_DECIMAL_POINT 0x181
 
 #endif
--- dist/specconstants.ts
+++ dist/specconstants.ts
@@ -2993,8 +2993,10 @@ export enum SettingsEvent {
 export const SRV_SEVEN_SEGMENT_DISPLAY = 0x196158f7
 export enum SevenSegmentDisplayReg {
     /**
-     * Read-write bytes. Each byte encodes the display status of a digit using a ``GFEDCBA`` encoding.
-     * If incoming ``digits`` data is smaller than available, the remaining digits will be cleared. Thus, sending an empty ``digits`` payload clears the screen.
+     * Read-write bytes. Each byte encodes the display status of a digit using,
+     * where bit 0 encodes segment `A`, bit 1 encodes segments `B`, ..., bit 6 encodes segments `G`, and bit 7 encodes the decimal point (if present).
+     * If incoming ``digits`` data is smaller than `digit_count`, the remaining digits will be cleared.
+     * Thus, sending an empty ``digits`` payload clears the screen.
      *
      * ```
      * const [digits] = jdunpack<[Uint8Array]>(buf, "b")
@@ -3012,7 +3014,8 @@ export enum SevenSegmentDisplayReg {
     Brightness = 0x1,
 
     /**
-     * Read-write bool (uint8_t). Turn on or off the column leds in of the segment. If the column leds is not supported, the value remains false.
+     * Read-write bool (uint8_t). Turn on or off the column LEDs (separating minutes from hours, etc.) in of the segment.
+     * If the column LEDs is not supported, the value remains false.
      *
      * ```
      * const [doubleDots] = jdunpack<[number]>(buf, "u8")
@@ -3027,7 +3030,16 @@ export enum SevenSegmentDisplayReg {
      * const [digitCount] = jdunpack<[number]>(buf, "u8")
      * ```
      */
-    DigitCount = 0x80,
+    DigitCount = 0x180,
+
+    /**
+     * Constant bool (uint8_t). True if decimal points are available (on all digits).
+     *
+     * ```
+     * const [decimalPoint] = jdunpack<[number]>(buf, "u8")
+     * ```
+     */
+    DecimalPoint = 0x181,
 }
 
 // Service: Soil moisture

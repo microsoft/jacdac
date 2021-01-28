eb38f55d642f1a18c95ec2a5da908054f31ea23e
pelikhan <pelikhan@users.noreply.github.com>
2021-01-28 21:30:55 +0000

    Apply automatic changes

--- dist/specconstants.ts
+++ dist/specconstants.ts
@@ -970,6 +970,46 @@ export enum ECO2Reg {
     Variant = 0x107,
 }
 
+// Service: Heart Rate
+export const SRV_HEART_RATE = 0x166c6dc4
+
+export enum HeartRateVariant { // uint32_t
+    Finger = 0x1,
+    Chest = 0x2,
+    Wrist = 0x3,
+    Pump = 0x4,
+    WebCam = 0x5,
+}
+
+export enum HeartRateReg {
+    /**
+     * Read-only bpm u16.16 (uint32_t). The estimated heart rate.
+     *
+     * ```
+     * const [heartRate] = jdunpack<[number]>(buf, "u16.16")
+     * ```
+     */
+    HeartRate = 0x101,
+
+    /**
+     * Read-only bpm u16.16 (uint32_t). The estimated error on the reported sensor data.
+     *
+     * ```
+     * const [heartRateError] = jdunpack<[number]>(buf, "u16.16")
+     * ```
+     */
+    HeartRateError = 0x106,
+
+    /**
+     * Constant Variant (uint32_t). The type of physical sensor
+     *
+     * ```
+     * const [variant] = jdunpack<[HeartRateVariant]>(buf, "u32")
+     * ```
+     */
+    Variant = 0x107,
+}
+
 // Service: Humidity
 export const SRV_HUMIDITY = 0x16c810b8
 export enum HumidityReg {

ea913f375b8f92495e39277c2b2e1d244f21439f
pelikhan <pelikhan@users.noreply.github.com>
2021-02-04 19:46:05 +0000

    Apply automatic changes

--- dist/specconstants.ts
+++ dist/specconstants.ts
@@ -1943,6 +1943,35 @@ export enum MicrophoneReg {
     SamplingPeriod = 0x80,
 }
 
+// Service: MIDI output
+export const SRV_M_IDIOUTPUT = 0x1a848cd7
+export enum MIDIOutputReg {
+    /**
+     * Read-write bool (uint8_t). Opens or closes the port to the MIDI device
+     *
+     * ```
+     * const [enabled] = jdunpack<[number]>(buf, "u8")
+     * ```
+     */
+    Enabled = 0x1,
+}
+
+export enum MIDIOutputCmd {
+    /**
+     * No args. Clears any pending send data that has not yet been sent from the MIDIOutput's queue.
+     */
+    Clear = 0x80,
+
+    /**
+     * Argument: data bytes. Enqueues the message to be sent to the corresponding MIDI port
+     *
+     * ```
+     * const [data] = jdunpack<[Uint8Array]>(buf, "b")
+     * ```
+     */
+    Send = 0x81,
+}
+
 // Service: Model Runner
 export const SRV_MODEL_RUNNER = 0x140f9a78
 

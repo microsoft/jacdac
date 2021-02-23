74964ac7dc9e204cb3449b00177a9ef550abb339
mmoskal <mmoskal@users.noreply.github.com>
2021-02-23 22:20:56 +0000

    Apply automatic changes

--- dist/specconstants.ts
+++ dist/specconstants.ts
@@ -318,6 +318,72 @@ export enum SensorAggregatorReg {
     CurrentSample = 0x101,
 }
 
+// Service: Arcade screen
+export const SRV_ARCADE_SCREEN = 0x16fa36e5
+
+export enum ArcadeScreenDisplayFlags { // uint8_t
+    ColumnMajor = 0x0,
+    RowMajor = 0x1,
+    Upscale2x = 0x2,
+}
+
+export enum ArcadeScreenCmd {
+    /**
+     * No args. Announces display capabilities and logical size
+     * (320x240 screen with `Upscale2x` will report 160x120).
+     */
+    Announce = 0x0,
+
+    /**
+     * report Announce
+     * ```
+     * const [flags, bitsPerPixel, width, height] = jdunpack<[ArcadeScreenDisplayFlags, number, number, number]>(buf, "u8 u8 u16 u16")
+     * ```
+     */
+
+    /**
+     * Sets the update window for subsequent `set_pixels` commands.
+     *
+     * ```
+     * const [x, y, width, height] = jdunpack<[number, number, number, number]>(buf, "u16 u16 u16 u16")
+     * ```
+     */
+    StartUpdate = 0x81,
+
+    /**
+     * Argument: pixels bytes. Set pixels in current window, according to current palette.
+     *
+     * ```
+     * const [pixels] = jdunpack<[UInt8Array]>(buf, "b")
+     * ```
+     */
+    SetPixels = 0x83,
+}
+
+export enum ArcadeScreenReg {
+    /**
+     * Read-write ratio uint8_t. Set backlight brightness.
+     * If set to `0` the display may go to sleep.
+     *
+     * ```
+     * const [brightness] = jdunpack<[number]>(buf, "u8")
+     * ```
+     */
+    Brightness = 0x1,
+
+    /**
+     * The current palette.
+     * The color entry repeats `1 << bits_per_pixel` times.
+     * This register may be write-only.
+     *
+     * ```
+     * const [rest] = jdunpack<[([number, number, number])[]]>(buf, "r: u8 u8 u8 x[1]")
+     * const [blue, green, red] = rest[0]
+     * ```
+     */
+    Palette = 0x80,
+}
+
 // Service: Bootloader
 export const SRV_BOOTLOADER = 0x1ffa9948
 

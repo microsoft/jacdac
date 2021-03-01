ba98fb2218cc5ff7153ff84504f640a0d863cc88
Tom Ball <tballmsft@users.noreply.github.com>
2021-02-28 19:45:50 -0800

    add jsep.d.ts as it is not in @typings

--- spectool/jdtest.d.ts
+++ spectool/jdtest.d.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/triple-slash-reference */
 /// <reference path="jdspec.d.ts" />
+/// <reference path="jsep.d.ts" />
 
 declare namespace jdtest {
 
@@ -43,8 +44,7 @@ declare namespace jdtest {
     interface UnitTest {
         description: string;
         prompt: string;
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        commands: any[];  // any -> jsep.Expression
+        commands: jsep.Expression[];  // any -> jsep.Expression
     }
 
     interface TestFunctionDescription {

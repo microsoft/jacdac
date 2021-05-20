acd7f4976c0641edbc83609d388a747a787d0af2
Tom Ball <tballmsft@users.noreply.github.com>
2021-05-20 13:41:30 -0700

    refactoring for vm roles (#385)
    * move spec lookup logic to utils
    
    * generalize lookup to support role-> service mapping
    
    * some more refactoring
    
    * ugh... imports
    
    * try again
    
    * fix lookup bug
    
    * remove console
    
    * generalize collection of registers/events

--- spectool/jdtest.ts
+++ spectool/jdtest.ts
@@ -1,12 +1,6 @@
-/* eslint-disable @typescript-eslint/triple-slash-reference */
-/// <reference path="jdspec.d.ts" />
-/// <reference path="jdtest.d.ts" />
-
 import { 
-    parseIntFloat, 
-    getRegister, 
     exprVisitor,
-    isBoolOrNumericFormat
+    SpecSymbolResolver
 } from "./jdutils"
 import { getTestCommandFunctions, getTestExpressionFunctions } from "./jdtestfuns"
 import jsep from "jsep"
@@ -44,6 +38,7 @@ export function parseSpecificationTestMarkdownToJSON(
     let currentTest: jdtest.TestSpec = null
     let testHeading = ""
     let testPrompt = ""
+    const symbolResolver = new SpecSymbolResolver(spec, undefined, supportedExpressions, jsep, (e) => error(e))
 
     try {
         for (const line of filecontent.split(/\n/)) {
@@ -54,7 +49,7 @@ export function parseSpecificationTestMarkdownToJSON(
         error("exception: " + e.message)
     }
 
-    if (currentTest) finishTest()
+    if (currentTest) finishTest(symbolResolver)
 
     if (errors.length) info.errors = errors
 
@@ -91,7 +86,7 @@ export function parseSpecificationTestMarkdownToJSON(
                     else 
                         error("use ## to start a test, not #")
                 } else if (hd == "##") {
-                    if (currentTest) finishTest()
+                    if (currentTest) finishTest(symbolResolver)
                     testHeading = cont.trim()
                 }
             } else {
@@ -102,10 +97,6 @@ export function parseSpecificationTestMarkdownToJSON(
             if (!expanded) return
             processCommand(expanded)
         }
-    }
-
-    function argsRequiredOptional(args: any[], optional: boolean = false) {
-        return args.filter(a => !optional && typeof(a) === "string" || optional && typeof(a) === "object")
     }
 
     function processCommand(expanded: string) {
@@ -126,123 +117,20 @@ export function parseSpecificationTestMarkdownToJSON(
             testHeading = ""
             testPrompt = ""
         }
-        const call = /^([a-zA-Z]\w*)\(.*\)$/.exec(expanded)
-        if (!call) {
-            error(
-                `a command must be a call to a registered test function (JavaScript syntax)`
-            )
-            return
-        }
-        const [, callee] = call
-        const testCommandFunctions = getTestCommandFunctions();
-        const cmdIndex = testCommandFunctions.findIndex(r => callee == r.id)
-        if (cmdIndex < 0) {
-            error(`${callee} is not a registered test command function.`)
-            return
-        }
-        const root: jsep.CallExpression = <jsep.CallExpression>jsep(expanded)
-        if (
-            !root ||
-            !root.type ||
-            root.type != "CallExpression" ||
-            !root.callee ||
-            !root.arguments
-        ) {
-            error(`a command must be a call expression in JavaScript syntax`)
-        } else {
-            // check for unsupported expression types
-            exprVisitor(null, root, (p, c) => {
-                if (supportedExpressions.indexOf(c.type) < 0)
-                    error(`Expression of type ${c.type} not currently supported`)
-            })
-            // check arguments
-            const command = testCommandFunctions[cmdIndex]
-            const minArgs = argsRequiredOptional(command.args).length
-            const maxArgs = command.args.length
-            if (root.arguments.length < minArgs)
-                error(
-                    `${callee} expects at least ${minArgs} arguments; got ${root.arguments.length}`
-                )
-            else if (root.arguments.length > maxArgs) {
-                error(
-                    `${callee} expects at most ${maxArgs} arguments; got ${root.arguments.length}`
-                )
-            }
-            else {
-                // deal with optional arguments
-                let newExpressions: jsep.Expression[] = []
-                for(let i = root.arguments.length; i<command.args.length;i++) {
-                    let [name, def] = command.args[i] as [string, any] 
-                    const lit: jsep.Literal = {
-                        type: "Literal",
-                        value: def,
-                        raw: def.toString(),
-                    }
-                    newExpressions.push(lit)
-                }
-                root.arguments = root.arguments.concat(newExpressions)
-                // type checking of arguments.
-                processArguments(command, root.arguments);
-                // check all calls in subexpressions
-                processCalls(command, root.arguments)
-            }
+ 
+        const ret = symbolResolver.processLine(expanded, getTestCommandFunctions());
+
+        if (ret) {
+            const [command, root] = ret
+            // check all calls in subexpressions
+            processCalls(command, root)
             currentTest.testCommands.push({ prompt: testPrompt, call: root })
             testPrompt = ""
         }
 
-        function processArguments(command: jdtest.TestFunctionDescription, args: jsep.Expression[]) {
-            let eventSymTable: jdspec.PacketInfo[] = []
-            args.forEach((arg, a) => {
-                let argType = command.args[a]
-                if (typeof(argType) === "object")
-                    argType = command.args[a][0]
-                if (argType === "register" || argType === "event") {
-                   if (arg.type !== "Identifier")
-                        error(
-                            `${callee} expects a ${argType} in argument position ${a + 1}`
-                        )
-                   else if (argType === "event" && a === 0) { 
-                        let pkt = lookupEvent(arg)
-                        if (pkt && eventSymTable.indexOf(pkt) === -1)
-                        eventSymTable.push(pkt)
-                   } else if (argType === "register") {
-                        try {
-                            lookupRegister((arg as jsep.Identifier).name, "")
-                        } catch (e) {
-                            error(e.message)
-                        }
-                   }
-                } else if (argType === "events") {
-                    if (arg.type != 'ArrayExpression')
-                        error(`events function expects a list of service events`)
-                    else {
-                        (arg as jsep.ArrayExpression).elements.forEach(lookupEvent)
-                    }
-                } else if (argType === "number" || argType === "boolean") {
-                    exprVisitor(root, arg, (p, c) => {
-                        if (p.type !== 'MemberExpression' && c.type === 'Identifier') {
-                            lookupReplace(eventSymTable, p, c as jsep.Identifier)
-                        } else if (c.type === 'ArrayExpression') {
-                            error(
-                                `array expression not allowed in this context`
-                            )
-                        } else if (c.type === 'MemberExpression') {
-                            const member = c as jsep.MemberExpression;
-                            // A member expression must be of form id1.id2
-                            if (member.object.type !== 'Identifier' || member.property.type !== 'Identifier' || member.computed) {
-                                error('property access must be of form id.property')
-                            } else {
-                                lookupReplace(eventSymTable, p, c as jsep.MemberExpression)
-                            }
-                        }
-                    })
-                } else {
-                    error(`unexpected argument type (${argType}) in jdtestfuns.ts`)
-                }
-            })
-        }
-
-        function processCalls(command: jdtest.TestFunctionDescription, args: jsep.Expression[]) {
+        // this checking is specific to test functions (for now)
+        function processCalls(command: jdtest.TestFunctionDescription, root: jsep.CallExpression) {
+            const args = root.arguments
             const testExpressionFunctions = getTestExpressionFunctions()
             args.forEach((arg, a) => {
                 const argType = command.args[a]
@@ -274,104 +162,17 @@ export function parseSpecificationTestMarkdownToJSON(
                     const expected = tef.args.length
                     if (expected !== callExpr.arguments.length)
                         error(
-                            `${callee} expects ${expected} arguments; got ${callExpr.arguments.length}`
+                            `Expected ${expected} arguments; got ${callExpr.arguments.length}`
                         )
                 })
             })
         }
-
-        function lookupEvent(e: jsep.Expression) {
-            const events = spec.packets?.filter(pkt => pkt.kind == "event")
-            if (e.type !== 'Identifier') {
-                error(`event identifier expected`)
-            } else {
-                const id = (e as jsep.Identifier).name
-                const pkt = events.find(p => p.name === id)
-                if (!pkt) {
-                    error(`no event ${id} in specification`)
-                } else {
-                    if (currentTest.events.indexOf(id) < 0)
-                        currentTest.events.push(id)
-                    return pkt;
-                }
-            }
-            return null;
-        }
     }
 
-    function lookupRegister(root:string, fld:string)  {
-        let reg = getRegister(spec, root, fld)
-        if (reg.pkt && (!reg.fld && !isBoolOrNumericFormat(reg.pkt.packFormat) ||
-                        reg.fld && reg.fld.type && !isBoolOrNumericFormat(reg.fld.type)))
-            error("only bool/numeric registers allowed in tests")
-        // if (!fld && regField.pkt.fields.length > 0)
-        //    error(`register ${root} has fields, but no field specified`)
-        if (currentTest.registers.indexOf(root) < 0)
-            currentTest.registers.push(root)
-    }
-
-    function lookupReplace(events: jdspec.PacketInfo[], parent: jsep.Expression, child: jsep.Identifier | jsep.MemberExpression) {
-        if (Array.isArray(parent)) {
-            let replace = lookup(events, parent, child)
-            parent.forEach(i => {
-                if (parent[i] === child)
-                    parent[i] = replace
-            })
-        } else {
-            // don't process identifiers that are callees of CallExpression
-            if (parent?.type === "CallExpression" && child === (<jsep.CallExpression>parent).callee)
-                return;
-            let replace = lookup(events, parent, child)
-            if (replace) {
-                Object.keys(parent).forEach(k => {
-                    if ((parent as any)[k] === child)
-                        (parent as any)[k] = replace
-                })
-            }
-        }
-
-        function lookup(events: jdspec.PacketInfo[], parent: jsep.Expression, child: jsep.Identifier | jsep.MemberExpression) {
-            try {
-                try {
-                    let [root,fld] = toName()
-                    const val = parseIntFloat(spec, fld ? `${root}.${fld}` : root)
-                    const lit: jsep.Literal = {
-                        type: "Literal",
-                        value: val,
-                        raw: val.toString(),
-                    }
-                    return lit
-                } catch (e) {
-                    let [root,fld] = toName()
-                    lookupRegister(root, fld)
-                }
-            } catch (e) {
-                if (events.length > 0) {
-                    let [root,fld] = toName()
-                    let pkt = events.find(pkt => pkt.name === root)
-                    if (!pkt)
-                        error(`event ${root} not bound correctly`)
-                    else if (!fld && pkt.fields.length > 0)
-                        error(`event ${root} has fields, but no field specified`)
-                    else if (fld && !pkt.fields.find(f => f.name === fld))
-                        error(`Field ${fld} of event ${root} not found in specification`)
-                } else {
-                    error(e.message)
-                }
-            }
-            return undefined
-            function toName() {
-                if (child.type !== 'MemberExpression')
-                    return [child.name, ""];
-                else {
-                    return [(child.object as jsep.Identifier).name,
-                            (child.property as jsep.Identifier).name]
-                }
-            }
-        }
-    }
-
-    function finishTest() {
+    function finishTest(s: SpecSymbolResolver) {
+        currentTest.registers = s.registers
+        currentTest.events = s.events
+        s.reset()
         info.tests.push(currentTest)
         currentTest = null
     }
--- spectool/jdutils.ts
+++ spectool/jdutils.ts
@@ -1,6 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/triple-slash-reference
-/// <reference path="jdspec.d.ts" />
-
 export function isBoolOrNumericFormat(fmt: string) {
     return fmt === "bool" || /^[ui]\d+/i.test(fmt)
 }
@@ -81,6 +78,277 @@ export function parseIntFloat(
     if (!en.members.hasOwnProperty(ww[1]))
         throw new Error(`${ww[1]} is not a member of ${ww[0]}`)
     return en.members[ww[1]] || 0
+}
+
+// static resolution of accesses to service specification
+
+export class SpecSymbolResolver {
+    registers: string[];
+    events: string[];
+    
+    constructor(private readonly spec: jdspec.ServiceSpec, 
+                private readonly role2spec: (role: string) => jdspec.ServiceSpec,
+                private readonly supportedExpressions: jsep.ExpressionType[],
+                private readonly parser: (line: string | jsep.Expression) => jsep.Expression,
+                private readonly error: (m:string) => void) {
+        this.reset()
+    }
+
+    reset() {
+        this.registers = []
+        this.events = []
+    }
+
+    processLine(line: string, funs: jdtest.TestFunctionDescription[]):
+            [jdtest.TestFunctionDescription, jsep.CallExpression] {
+        const call = /^([a-zA-Z]\w*)\(.*\)$/.exec(line)
+        if (!call) {
+            this.error(
+                `a command must be a call to a registered function (JavaScript syntax)`
+            )
+            return undefined
+        }
+        const [, callee] = call
+        const cmdIndex = funs.findIndex(r => callee == r.id)
+        if (cmdIndex < 0) {
+            this.error(`${callee} is not a registered function.`)
+            return undefined
+        }
+        const root: jsep.CallExpression = <jsep.CallExpression>this.parser(line)
+        if (
+            !root ||
+            !root.type ||
+            root.type != "CallExpression" ||
+            !root.callee ||
+            !root.arguments
+        ) {
+            this.error(`a command must be a call expression in JavaScript syntax`)
+            return undefined
+        }
+        // check for unsupported expression types
+        exprVisitor(null, root, (p, c) => {
+            if (this.supportedExpressions.indexOf(c.type) < 0)
+                this.error(`Expression of type ${c.type} not currently supported`)
+        })
+        // check arguments
+        const command = funs[cmdIndex]
+        const minArgs = argsRequiredOptional(command.args).length
+        const maxArgs = command.args.length
+        if (root.arguments.length < minArgs) {
+            this.error(
+                `${callee} expects at least ${minArgs} arguments; got ${root.arguments.length}`
+            )
+            return undefined
+        } else if (root.arguments.length > maxArgs) {
+            this.error(
+                `${callee} expects at most ${maxArgs} arguments; got ${root.arguments.length}`
+            )
+            return undefined
+        }
+        // deal with optional arguments
+        let newExpressions: jsep.Expression[] = []
+        for(let i = root.arguments.length; i<command.args.length;i++) {
+            let [name, def] = command.args[i] as [string, any] 
+            const lit: jsep.Literal = {
+                type: "Literal",
+                value: def,
+                raw: def.toString(),
+            }
+            newExpressions.push(lit)
+        }
+        root.arguments = root.arguments.concat(newExpressions)       
+        // type checking of arguments.
+        this.processArguments(command, root);
+        return [command, root]
+
+        function argsRequiredOptional(args: any[], optional: boolean = false) {
+            return args.filter(a => !optional && typeof(a) === "string" || optional && typeof(a) === "object")
+        }
+    }
+
+    private processArguments(command: jdtest.TestFunctionDescription, root: jsep.CallExpression) {
+        const args = root.arguments
+        const eventSymTable: jdspec.PacketInfo[] = []
+        args.forEach((arg, a) => {
+            let argType = command.args[a]
+            
+            if (typeof(argType) === "object")
+                argType = command.args[a][0]
+            
+            if (argType === "register" || argType === "event" || argType == "Identifier") {
+                if (argType == "Identifier") {
+                    this.check(arg,"Identifier")
+                } else if (argType === "event" && a === 0) { 
+                    let pkt = this.lookupEvent(arg)
+                    if (pkt && eventSymTable.indexOf(pkt) === -1)
+                    eventSymTable.push(pkt)
+                } else if (argType === "register") {
+                    try {
+                        this.lookupRegister(arg)
+                    } catch (e) {
+                        this.error(e.message)
+                    }
+                }
+            } else if (argType === "events") {
+                if (arg.type != 'ArrayExpression')
+                    this.error(`events function expects a list of service events`)
+                else {
+                    (arg as jsep.ArrayExpression).elements.forEach((e) => this.lookupEvent(e))
+                }
+            } else if (argType === "number" || argType === "boolean") {
+                exprVisitor(root, arg, (p, c) => {
+                    // TODO
+                    if (p.type !== 'MemberExpression' && c.type === 'Identifier') {
+                        this.lookupReplace(eventSymTable, p, c as jsep.Identifier)
+                    } else if (c.type === 'ArrayExpression') {
+                        this.error(
+                            `array expression not allowed in this context`
+                        )
+                    } else if (c.type === 'MemberExpression') {
+                        const member = c as jsep.MemberExpression;
+                        // A member expression must be of form <Identifier>.<memberExpression|Identifier>
+                        if (member.object.type !== 'Identifier' || member.computed) {
+                            this.error('property access must be of form id.property')
+                        } else {
+                            this.lookupReplace(eventSymTable, p, c as jsep.MemberExpression)
+                        }
+                    }
+                })
+            } else {
+                this.error(`unexpected argument type (${argType})`)
+            }
+        })
+    }
+
+    // TODO: OR
+    private check(e: jsep.Expression, type: string) {
+        if (e.type !== type)
+            this.error(`expected ${type}; got ${e.type}`)
+    }
+
+    private specResolve(e: jsep.Expression): [string, jdspec.ServiceSpec, jsep.Expression] {
+        if (this.spec) {
+            return [this.spec.shortName, this.spec, e]
+        }
+        // otherwise, we must have a memberexpression at top-level
+        // where the object references a role variable or specification shortName
+        this.check(e,"MemberExpression");
+        this.check((e as jsep.MemberExpression).object, "Identifier");
+        if (this.role2spec) {
+            const obj = (e as jsep.MemberExpression).object as jsep.Identifier
+            if (!this.role2spec(obj.name)) {
+                this.error(`no specification found for ${obj.name}`)
+            }
+            return [obj.name, this.role2spec(obj.name), (e as jsep.MemberExpression).property]
+        }
+    }
+
+    private destructAccessPath(e: jsep.Expression, expectIdentifier = false) {
+        if (e.type === "Identifier") {
+            return [(e as jsep.Identifier).name, ""]
+        } else if (!expectIdentifier && e.type === "MemberExpression") {
+            let object = (e as jsep.MemberExpression).object as jsep.Identifier
+            let property = (e as jsep.MemberExpression).property as jsep.Identifier
+            this.check(object, "Identifier")
+            this.check(property, "Identifier");
+            return [object.name, property.name]
+        } else {
+            if (!expectIdentifier)
+                this.error(`expected Identifier or MemberExpression; got ${e.type}`)
+            else 
+                this.error(`expected Identifier; got ${e.type}`)
+            return undefined
+        }
+    }
+    
+    private lookupEvent(e: jsep.Expression) {
+        let [role,spec,rest] = this.specResolve(e)
+        let [id, _] = this.destructAccessPath(rest,true)
+        const events = spec.packets?.filter(pkt => pkt.kind == "event")
+        const pkt = events.find(p => p.name === id)
+        if (!pkt) {
+            this.error(`no event ${id} in specification`)
+            return undefined;
+        } else {
+            let ev = `${role}.${id}`
+            if (this.events.indexOf(ev) < 0)
+                this.events.push(ev)
+            return pkt;
+        }
+    }
+
+    private lookupRegister(e: jsep.Expression)  {
+        let [role,spec,rest] = this.specResolve(e)
+        let [root, fld] = this.destructAccessPath(rest)
+        this.lookupRegisterRaw(spec, root, fld)
+        let reg = `${role}.${root}`
+        if (this.registers.indexOf(reg) < 0)
+            this.registers.push(reg)
+    }
+
+    private lookupRegisterRaw(spec: jdspec.ServiceSpec, root: string, fld: string)  {
+        let reg = getRegister(spec, root, fld)
+        if (reg.pkt && (!reg.fld && !isBoolOrNumericFormat(reg.pkt.packFormat) ||
+                        reg.fld && reg.fld.type && !isBoolOrNumericFormat(reg.fld.type)))
+            this.error("only bool/numeric registers allowed")
+        // if (!fld && regField.pkt.fields.length > 0)
+        //    error(`register ${root} has fields, but no field specified`)
+    }
+
+
+    private lookupReplace(events: jdspec.PacketInfo[], parent: jsep.Expression, child: jsep.Identifier | jsep.MemberExpression) {
+        if (Array.isArray(parent)) {
+            let replace = this.lookup(events, parent, child)
+            parent.forEach(i => {
+                if (parent[i] === child)
+                    parent[i] = replace
+            })
+        } else {
+            // don't process identifiers that are callees of CallExpression
+            if (parent?.type === "CallExpression" && child === (<jsep.CallExpression>parent).callee)
+                return;
+            let replace = this.lookup(events, parent, child)
+            if (replace) {
+                Object.keys(parent).forEach(k => {
+                    if ((parent as any)[k] === child)
+                        (parent as any)[k] = replace
+                })
+            }
+        }
+    }
+
+    private lookup(events: jdspec.PacketInfo[], parent: jsep.Expression, child: jsep.Identifier | jsep.MemberExpression) {
+        let [role, spec,rest] = this.specResolve(child)
+        let [root, fld] = this.destructAccessPath(rest)
+        try {
+            try {
+                const val = parseIntFloat(spec, fld ? `${root}.${fld}` : root)
+                const lit: jsep.Literal = {
+                    type: "Literal",
+                    value: val,
+                    raw: val.toString(),
+                }
+                return lit
+            } catch (e) {
+                this.lookupRegisterRaw(spec, root, fld)
+                let reg = `${role}.${root}`
+                if (this.registers.indexOf(reg) < 0)
+                    this.registers.push(reg)
+            }
+        } catch (e) {
+            if (events.length > 0) {
+                let pkt = events.find(pkt => pkt.name === root)
+                if (!pkt)
+                    this.error(`event ${root} not bound correctly`)
+                else if (!fld && pkt.fields.length > 0)
+                    this.error(`event ${root} has fields, but no field specified`)
+                else if (fld && !pkt.fields.find(f => f.name === fld))
+                    this.error(`Field ${fld} of event ${root} not found in specification`)
+            } else {
+                this.error(e.message)
+            }
+        }
+    }
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any

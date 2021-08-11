5d961eb68b94b8a08cd2a3f54143d2f8e988ba57
pelikhan <pelikhan@users.noreply.github.com>
2021-08-11 18:59:56 +0000

    Apply automatic changes

--- dist/specconstants.ts
+++ dist/specconstants.ts
@@ -1264,6 +1264,35 @@ export enum ControlReg {
     FirmwareUrl = 0x188,
 }
 
+// Service: Dimmer
+export const SRV_DIMMER = 0x1fb02645
+
+export enum DimmerVariant { // uint8_t
+    Light = 0x1,
+    Fan = 0x2,
+    Pump = 0x3,
+}
+
+export enum DimmerReg {
+    /**
+     * Read-write ratio u0.16 (uint16_t). The intensity of the current. Set to ``0`` to turn off completely the current.
+     *
+     * ```
+     * const [intensity] = jdunpack<[number]>(buf, "u0.16")
+     * ```
+     */
+    Intensity = 0x1,
+
+    /**
+     * Constant Variant (uint8_t). The type of physical device
+     *
+     * ```
+     * const [variant] = jdunpack<[DimmerVariant]>(buf, "u8")
+     * ```
+     */
+    Variant = 0x107,
+}
+
 // Service: Distance
 export const SRV_DISTANCE = 0x141a6b8a
 
